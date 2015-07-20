cdca545e3220aa0c0ffbfee9abebfb95656afbe5
Johannes Schickling <schickling.j@gmail.com>
2015-07-20 11:45:14 +0200

    init framework

---
+++ Device/Device.h
@@ -0,0 +1,19 @@
+//
+//  Device.h
+//  Device
+//
+//  Created by Johannes Schickling on 7/20/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Device.
+FOUNDATION_EXPORT double DeviceVersionNumber;
+
+//! Project version string for Device.
+FOUNDATION_EXPORT const unsigned char DeviceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Device/PublicHeader.h>
+
+
