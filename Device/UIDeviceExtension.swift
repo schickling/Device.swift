2a71f9a76f8430bc6f7011a84d7a6ff3362fd17a
Cody Winton <cody@credntia.com>
2017-01-12 11:57:05 -0600

    Refactored Device identifier init
    By adding the variable `identifiers`, this ensures that, when a new device type is added, the compiler will prompt the addition of the possible identifiers
    This also makes DeviceType more testable

--- Device/UIDeviceExtension.swift
+++ Device/UIDeviceExtension.swift
@@ -80,53 +80,6 @@ public enum DeviceType: String, EnumProtocol {
 		return DeviceType(identifier: identifier)
 	}
 	
-	/** Creates a device type
-	- parameters:
-		- identifier: The identifier of the device
-	- returns: The device type based on the provided identifier
-	*/
-	internal init(identifier: String) {
-		switch identifier {
-		case "i386", "x86_64": self = .simulator
-		case "iPhone1,1": self = .iPhone2G
-		case "iPhone1,2": self = .iPhone3G
-		case "iPhone2,1": self = .iPhone3GS
-		case "iPhone3,1", "iPhone3,2", "iPhone3,3": self = .iPhone4
-		case "iPhone4,1": self = .iPhone4S
-		case "iPhone5,1", "iPhone5,2": self = .iPhone5
-		case "iPhone5,3", "iPhone5,4": self = .iPhone5C
-		case "iPhone6,1", "iPhone6,2": self = .iPhone5S
-		case "iPhone7,1": self = .iPhone6Plus
-		case "iPhone7,2": self = .iPhone6
-		case "iPhone8,2": self = .iPhone6SPlus
-		case "iPhone8,1": self = .iPhone6S
-		case "iPhone8,4": self = .iPhoneSE
-		case "iPhone9,1", "iPhone9,3": self = .iPhone7
-		case "iPhone9,2", "iPhone9,4": self = .iPhone7Plus
-			
-		case "iPod1,1": self = .iPodTouch1G
-		case "iPod2,1": self = .iPodTouch2G
-		case "iPod3,1": self = .iPodTouch3G
-		case "iPod4,1": self = .iPodTouch4G
-		case "iPod5,1": self = .iPodTouch5G
-			
-		case "iPad1,1", "iPad1,2": self = .iPad
-		case "iPad2,1", "iPad2,2", "iPad2,3", "iPad2,4": self = .iPad2
-		case "iPad2,5", "iPad2,6", "iPad2,7": self = .iPadMini
-		case "iPad3,1", "iPad3,2", "iPad3,3": self = .iPad3
-		case "iPad3,4", "iPad3,5", "iPad3,6": self = .iPad4
-		case "iPad4,1", "iPad4,2", "iPad4,3": self = .iPadAir
-		case "iPad4,4", "iPad4,5", "iPad4,6": self = .iPadMiniRetina
-		case "iPad4,7", "iPad4,8": self = .iPadMini3
-		case "iPad5,1", "iPad5,2": self = .iPadMini4
-		case "iPad5,3", "iPad5,4": self = .iPadAir2
-		case "iPad6,3", "iPad6,4": self = .iPadPro9Inch
-		case "iPad6,7", "iPad6,8": self = .iPadPro12Inch
-			
-		default:
-			self = .notAvailable
-		}
-	}
 	
 	/// Returns the display name of the device type
 	var displayName: String {
@@ -167,6 +120,66 @@ public enum DeviceType: String, EnumProtocol {
 		case .simulator: return "Simulator"
 		case .notAvailable: return "Not Available"
 		}
+	}
+	
+	internal var identifiers: [String] {
+		
+		switch self {
+		case .notAvailable: return []
+		case .simulator: return ["i386", "x86_64"]
+		
+		case .iPhone2G: return ["iPhone1,1"]
+		case .iPhone3G: return ["iPhone1,2"]
+		case .iPhone3GS: return ["iPhone2,1"]
+		case .iPhone4: return ["iPhone3,1", "iPhone3,2", "iPhone3,3"]
+		case .iPhone4S: return ["iPhone4,1"]
+		case .iPhone5: return ["iPhone5,1", "iPhone5,2"]
+		case .iPhone5C: return ["iPhone5,3", "iPhone5,4"]
+		case .iPhone5S: return ["iPhone6,1", "iPhone6,2"]
+		case .iPhone6Plus: return ["iPhone7,1"]
+		case .iPhone6: return ["iPhone7,2"]
+		case .iPhone6S: return ["iPhone8,1"]
+		case .iPhone6SPlus: return ["iPhone8,2"]
+		case .iPhone7: return ["iPhone9,1", "iPhone9,3"]
+		case .iPhone7Plus: return ["iPhone9,2", "iPhone9,4"]
+		case .iPhoneSE: return ["iPhone8,4"]
+		
+		case .iPodTouch1G: return ["iPod1,1"]
+		case .iPodTouch2G: return ["iPod2,1"]
+		case .iPodTouch3G: return ["iPod3,1"]
+		case .iPodTouch4G: return ["iPod4,1"]
+		case .iPodTouch5G: return ["iPod5,1"]
+		
+		case .iPad: return ["iPad1,1", "iPad1,2"]
+		case .iPad2: return ["iPad2,1", "iPad2,2", "iPad2,3", "iPad2,4"]
+		case .iPad3: return ["iPad3,1", "iPad3,2", "iPad3,3"]
+		case .iPad4: return ["iPad3,4", "iPad3,5", "iPad3,6"]
+		case .iPadMini: return ["iPad2,5", "iPad2,6", "iPad2,7"]
+		case .iPadMiniRetina: return ["iPad4,4", "iPad4,5", "iPad4,6"]
+		case .iPadMini3: return ["iPad4,7", "iPad4,8"]
+		case .iPadMini4: return ["iPad5,1", "iPad5,2"]
+		case .iPadAir: return ["iPad4,1", "iPad4,2", "iPad4,3"]
+		case .iPadAir2: return ["iPad5,3", "iPad5,4"]
+		case .iPadPro9Inch: return ["iPad6,3", "iPad6,4"]
+		case .iPadPro12Inch: return ["iPad6,7", "iPad6,8"]
+		}
+	}
+	
+	/** Creates a device type
+	- parameter identifier: The identifier of the device
+	- returns: The device type based on the provided identifier
+	*/
+	internal init(identifier: String) {
+		
+		for device in DeviceType.all {
+			for deviceId in device.identifiers {
+				guard identifier == deviceId else { continue }
+				self = device
+				return
+			}
+		}
+		
+		self = .notAvailable
 	}
 }
 
